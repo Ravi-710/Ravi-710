cbba246a24ee10a9b642f812a42f2795f1c71d50
Ravi Kumar <98272814+Ravi-710@users.noreply.github.com>
2023-12-22 22:48:09 +0530

    Add files via upload

---
+++ ContactPage/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav className={`${styles.navigation} container`}>
+      <div className="logo">
+        <img src="/images/images.png" alt="Contact Form Logo" width='42px' height='42px' />
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
